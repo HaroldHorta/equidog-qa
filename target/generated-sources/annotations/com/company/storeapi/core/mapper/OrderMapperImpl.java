4ce67c3ee1d3b03f631e7b977c64d2df379b7906
Harold <hahorta@misena.edu.co>
2021-01-06 21:09:24 -0500

    make it better

--- target/generated-sources/annotations/com/company/storeapi/core/mapper/OrderMapperImpl.java
+++ target/generated-sources/annotations/com/company/storeapi/core/mapper/OrderMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-06T20:57:44-0500",
+    date = "2021-01-06T21:08:06-0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/com/company/storeapi/core/mapper/TicketMapperImpl.java
+++ target/generated-sources/annotations/com/company/storeapi/core/mapper/TicketMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-06T20:58:44-0500",
+    date = "2021-01-06T21:08:07-0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
