d24f2f1baef8e901e5a215530b67a2fccba137b7
Harold <hhorta@cidenet.com.co>
2021-03-24 00:20:15 -0500

    ajustes

--- target/generated-sources/annotations/com/company/storeapi/core/mapper/TicketMapperImpl.java
+++ target/generated-sources/annotations/com/company/storeapi/core/mapper/TicketMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-23T23:56:53-0500",
+    date = "2021-03-24T00:19:23-0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
